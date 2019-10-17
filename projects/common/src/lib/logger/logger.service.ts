ec5bc386d77e4d630c4ae59672a695a6c2b516aa
Richard Collier <ergosoftuk@gmail.com>
2019-10-17 11:11:27 +0100

    Added console logging

--- projects/common/src/lib/logger/logger.service.ts
+++ projects/common/src/lib/logger/logger.service.ts
@@ -8,22 +8,25 @@ export class LoggerService {
   constructor(private snackBar: MatSnackBar) { }
 
   info(message: string) {
+    console.info(message);
     this.snackBar.open(message, null, {duration: 3000, panelClass: 'logger-info', horizontalPosition: 'right'});
   }
 
   success(message: string) {
+    console.log(message);
     this.snackBar.open(message, null, {duration: 3000, panelClass: 'logger-success', horizontalPosition: 'right'});
   }
 
   error(message: string) {
+    console.error(message);
     this.snackBar.open(message, null, {duration: 3000, panelClass: 'logger-error', horizontalPosition: 'right'});
   }
 
   debug(message: string) {
-    console.log(message);
+    console.debug(message);
   }
 
   trace(message: string) {
-    console.log(message);
+    console.trace(message);
   }
 }
