87587f914bb3b64af391a1d9eeaebe808de2d84c
stuart <stuart@bearclawsolutions.co.uk>
2020-01-09 10:03:03 +0000

    Include the generic table module

--- projects/dds-angular8/src/public-api.ts
+++ projects/dds-angular8/src/public-api.ts
@@ -7,7 +7,7 @@ export {LayoutComponent} from './lib/layout/layout.component';
 export {MenuOption} from './lib/layout/models/MenuOption';
 
 export {SecurityModule} from './lib/security/security.module';
-export {CanActivateRouteGuard} from './lib/security/can-activate-route.guard'
+export {CanActivateRouteGuard} from './lib/security/can-activate-route.guard';
 
 export {LoggerModule} from './lib/logger/logger.module';
 export {LoggerService} from './lib/logger/logger.service';
@@ -17,3 +17,5 @@ export {UserManagerService} from './lib/user-manager/user-manager.service';
 
 export {CoreModule} from './lib/core/core.module';
 export {StateService} from './lib/core/state.service';
+
+export {GenericTableModule} from './lib/generic-table/generic-table.module';
