3f2bf11e01069c606e4a9923b761c9b31f4368e8
Richard Collier <ergosoftuk@gmail.com>
2020-06-02 20:47:29 +0100

    Better message handling in logger

--- projects/dds-angular8/logger/src/logger.service.ts
+++ projects/dds-angular8/logger/src/logger.service.ts
@@ -7,26 +7,36 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 export class LoggerService {
   constructor(private snackBar: MatSnackBar) { }
 
-  info(message: string) {
-    console.info(message);
-    this.snackBar.open(message, null, {duration: 3000, panelClass: 'logger-info', horizontalPosition: 'right'});
+  info(data: any) {
+    console.info(this.getText(data));
+    this.snackBar.open(this.getText(data), null, {duration: 3000, panelClass: 'logger-info', horizontalPosition: 'right'});
   }
 
-  success(message: string) {
-    console.log(message);
-    this.snackBar.open(message, null, {duration: 3000, panelClass: 'logger-success', horizontalPosition: 'right'});
+  success(data: any) {
+    console.log(this.getText(data));
+    this.snackBar.open(this.getText(data), null, {duration: 3000, panelClass: 'logger-success', horizontalPosition: 'right'});
   }
 
-  error(message: string) {
-    console.error(message);
-    this.snackBar.open(message, null, {duration: 3000, panelClass: 'logger-error', horizontalPosition: 'right'});
+  error(data: any) {
+    console.error(this.getText(data));
+    this.snackBar.open(this.getText(data), null, {duration: 3000, panelClass: 'logger-error', horizontalPosition: 'right'});
   }
 
-  debug(message: string) {
-    console.debug(message);
+  debug(data: any) {
+    console.debug(this.getText(data));
   }
 
-  trace(message: string) {
-    console.trace(message);
+  trace(data: any) {
+    console.trace(this.getText(data));
+  }
+
+  getText(data: any) {
+    if (data.statusText)
+      return data.statusText;
+
+    if (data instanceof Object)
+      return data.toString();
+
+    return data;
   }
 }
